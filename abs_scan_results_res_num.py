027978dbb5d4a3f40229b3386c3e1029381e3326
sergiupocol <sergiupocol7@gmail.com>
2020-04-27 21:27:50 -0400

    Generate residue identifier for use by OSPREY

---
+++ abs_scan_results_res_num.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.insert(0, '../pypdb')
+from pypdb import *
+from Bio.PDB import *
+# OSPREY identifies residues by their peptide chain and residue number
+# Given residue number from Alanine_Scanning_Binding_Energies_Result.csv
+# AND the peptide chain from the pdb file, generate the osprey code
+# The ABS Scan requires the specification of a LIGAND number
+
+pdb_id = '2pt1' # CHANGE HERE
+protein_name = 'FutA1'
+ligand_name = 'SO4'
+ligand_name_id = 'H_'+ligand_name
+pdb_file_name = pdb_id + '.pdb'
+ligand_res_number = 362 # SO4 in FutA1
+
+# Grab the peptide chain the ligand is bound to
+pdb_file = get_pdb_file(pdb_id, filetype='pdb', compression=False)
+
+
+parser = PDBParser()
+structure = parser.get_structure(protein_name, pdb_file_name)
+
+# Now goal is to get chain containing ligand residue
+# OSPREY requires a residue id which is comprised of
+#   a hetero-flag (non-standard residue), sequence identifier (order in chain)
+#   and an insertion code (for insertion mutations but since we're parsing wild
+#   type protein it will always be 'A')
+
+# Unfold residues then call get parent
+ligand_chain = ''
+ligand_id = (ligand_name_id, ligand_res_number, ' ')
+
+# Go through avery chain
+# if chain has a resiude w ligand_res_number
+# then grab the residue and set ligand_chain
+# " Residue object have the tuple (hetfield, resseq, icode) as id"
+for model in structure:
+    for chain in model:
+        if chain.has_id(ligand_id):
+            ligand_chain = chain.get_id()
+            break
+
+osprey_code = ligand_chain+ligan_name
+
+
+        
+
+
